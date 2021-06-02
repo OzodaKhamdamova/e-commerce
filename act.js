1082a321d1456043fbecf361311e70d51d99fe34
OzodaKhamdamova <78717275+OzodaKhamdamova@users.noreply.github.com>
2021-06-02 14:18:24 +0500

    home page

--- act.js
+++ act.js
@@ -1,13 +1,37 @@
-window.addEventListener('DOMContentLoaded' ,()=>{
-    let showModal= document.querySelector('.show')
-    let closeModal = document.querySelector('close')
-showModal.addEventListener("click" , ()=>{
-    document.querySelector('.form').classList.toggle('show')
-})
-closeModal.addEventListener('click' ,()=>{
-    document.querySelector('.form').classList.remove('show')
-})
+window.addEventListener('DOMContentLoaded',()=>{
+    let openBurger = document.querySelector('.burger')
+    let closeBurger = document.querySelector('.active-burger')
+
+    openBurger.addEventListener('click',()=>{
+        document.querySelector('.sitenav').classList.toggle('active-burger')
+        document.querySelector('.baskets-wrapper').classList.toggle('active-burger')
+        document.querySelector('.search-btn').classList.toggle('disabled-search')
+    })
+    document.querySelector('.search-btn')('click',()=>{
+        document.querySelector('.input-search').classList.toggle('search-enabled')
+    })
+    const burger = document.querySelector(".burger");
+
+    burger.addEventListener("click", function () {
+    const body = document.body;
+    body.classList.toggle("nav-open");
+});
 
 
-
 })
+feather.replace();
+
+const nextEl = document.getElementById("next");
+const previousEl = document.getElementById("previous");
+const sliderEl = document.getElementById("slider");
+nextEl.addEventListener("click", onNextClick);
+previousEl.addEventListener("click", onPreviousClick);
+
+function onNextClick() {
+  const imgWidth = sliderEl.offsetWidth;
+  sliderEl.scrollLeft += imgWidth;
+}
+function onPreviousClick() {
+  const imgWidth = sliderEl.offsetWidth;
+  sliderEl.scrollLeft -= imgWidth;
+}
